cdfa9b10481e947b2f5a40c672ceb408d8f15658
Abhishek H K <abhishek.h.k@walmart.com>
2026-01-18 14:54:11 +0530

    add admin page routes - add book and add bundle

---
+++ src/app/admin/books/create/page.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/common/Container/Container";
+import { BookForm } from "@/components/admin/BookForm/BookForm";
+
+export default function CreateBookPage() {
+    return (
+        <main>
+            <Container>
+                <BookForm />
+            </Container>
+        </main>
+    );
+}
